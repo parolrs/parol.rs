604c6206e3edaf3464b3f3f72bc562fff46fc437
Ogromny <ogromny@openmailbox.org>
2017-04-11 15:30:03 +0200

    Rewrited utils

--- src/utils.rs
+++ src/utils.rs
@@ -4,35 +4,16 @@ pub extern crate gtk;
 pub extern crate gdk_pixbuf;
 pub extern crate parolrs;
 
-pub use std::collections::HashMap;
-
+pub use self::gdk_pixbuf::{Pixbuf, PixbufLoader};
 pub use self::gtk::prelude::*;
 pub use self::gtk::{
     init, main_quit, main,
-    Window, WindowType,
-    HeaderBar,
-    Button,
-    Image,
-    Box, Orientation,
-    MessageDialog, MessageType,
-    ButtonsType,
-    DialogFlags,
-    TreeView, TreeViewColumn,
-    CellRendererText,
-    TreeViewGridLines,
-    ListStore,
-    Entry,
-    TreePath,
-    Menu,
-    MenuItem,
-    Label,
-    SelectionMode,
-    ScrolledWindow
+    Window, WindowType, HeaderBar, Button, Image, Box, Orientation, MessageDialog, MessageType,
+    ButtonsType, DialogFlags, TreeView, TreeViewColumn, CellRendererText, TreeViewGridLines,
+    ListStore, Entry, TreePath, Menu, MenuItem, Label, SelectionMode, ScrolledWindow
 };
 
-pub use self::gdk_pixbuf::{
-    Pixbuf, PixbufLoader
-};
+const COLUMN_NAME: [&'static str; 4] = ["Program", "Username", "Password", "Note"];
 
 macro_rules! clone {
     (@param _) =>
@@ -52,99 +33,71 @@ macro_rules! clone {
 }
 
 pub fn message_box(window: &Window, message_type: MessageType, button: ButtonsType, message: &str) -> MessageDialog {
-    MessageDialog::new(
-        Some(window),
-        DialogFlags::all(),
-        message_type,
-        button,
-        message
-    )
+    MessageDialog::new(Some(window), DialogFlags::all(), message_type, button, message)
 }
 
 pub fn ask_password(window: &Window, message: &str) -> String {
-    let input_password = MessageDialog::new(
-        Some(window), DialogFlags::all(), MessageType::Question, ButtonsType::OkCancel, message
-    );
-
+    let input_password = MessageDialog::new(Some(window), DialogFlags::all(), MessageType::Question, ButtonsType::OkCancel, message);
     let entry = Entry::new();
-    input_password.get_content_area().pack_start(&entry, true, false, 0);
 
+    input_password.get_content_area().pack_start(&entry, true, false, 0);
     input_password.show_all();
     input_password.set_size_request(200, 100);
 
-    if input_password.run() == -5 {
-        match entry.get_text() {
-            Some(password) => {
-                input_password.destroy();
-                return password
-            },
-            None => {
-                input_password.destroy();
-                panic!()
-            }
-        }
-    } else {
+    if input_password.run() != -5 {
+        panic!();
+    }
+
+    if let Some(password) = entry.get_text() {
         input_password.destroy();
+        return password;
+    } else {
         panic!()
     }
 }
 
 pub fn bytes_to_pixbuf(bytes: &[u8]) -> Pixbuf {
-    match PixbufLoader::new_with_type("png") {
-        Ok(p) => {
-            match p.loader_write(bytes) {
-                Ok(_) => {
-                    match p.close() {
-                        Ok(_) => {
-                            match p.get_pixbuf() {
-                                Some(pixbuf) => pixbuf,
-                                None => panic!("No pixbuf !"),
-                            }
-                        },
-                        Err(e) => panic!("{}", e),
-                    }
-                },
-                Err(e) => panic!("{}", e),
-            }
-        },
+    let pixbuf = match PixbufLoader::new_with_type("png") {
+        Ok(pixbuf) => pixbuf,
         Err(e) => panic!("{}", e),
+    };
+
+    if let Err(e) = pixbuf.loader_write(bytes) {
+        panic!("{}", e);
+    } else {
+        if let Err(e) = pixbuf.close() {
+            panic!("{}", e);
+        }
+    }
+
+    if let Some(pixbuf) = pixbuf.get_pixbuf() {
+        return pixbuf;
+    } else {
+        panic!("No pixbuf !");
     }
 }
 
 pub fn init_view(list_store: &ListStore) -> TreeView {
-    let column_name = ["Program", "Username", "Password", "Note"];
     let treeview = TreeView::new();
 
     for i in 0 .. 4 {
-        let column = TreeViewColumn::new();
         let cell   = CellRendererText::new();
         cell.set_alignment(0.5, 0.5);
         cell.set_property_editable(true);
-        let list_store = list_store.clone();
-        cell.connect_edited(move |_, tree_path, new_text| {
-            match list_store.get_iter(&tree_path) {
-                Some(iter) => list_store.set_value(&iter, i, &new_text.to_value()),
-                None => {}
-            };
-        });
+        cell.connect_edited(clone!(list_store => move |_, tree_path, new_text| {
+            if let Some(iter) = list_store.get_iter(&tree_path) {
+                list_store.set_value(&iter, i, &new_text.to_value());
+            }
+        }));
 
+        let column = TreeViewColumn::new();
         column.pack_start(&cell, true);
-        column.set_title(column_name[i as usize]);
+        column.set_title(COLUMN_NAME[i as usize]);
         column.add_attribute(&cell, "text", i as i32);
-        column.set_min_width(match i {
-            0 => 200,
-            1 => 160,
-            2 => 180,
-            _ => -1,
-        });
         column.set_resizable(true);
         column.set_alignment(0.5);
-        column.set_clickable(match i {
-            0 => true,
-            1 => true,
-            2 => true,
-            _ => false,
-        });
+        column.set_min_width(match i { 0 => 200, 1 => 160, 2 => 180, _ => -1 });
+        column.set_clickable(match i { 0...2 => true, _ => false });
 
         treeview.append_column(&column);
     }
@@ -153,53 +106,33 @@ pub fn init_view(list_store: &ListStore) -> TreeView {
     treeview.set_fixed_height_mode(true);
     treeview.get_selection().set_mode(SelectionMode::Multiple);
 
-    treeview
+    return treeview;
 }
 
 pub fn insert_row(model: &ListStore, data: [&str; 4]) {
     let iter = model.append();
-    model.set_value(&iter, 0, &data[0].to_value());
-    model.set_value(&iter, 1, &data[1].to_value());
-    model.set_value(&iter, 2, &data[2].to_value());
-    model.set_value(&iter, 3, &data[3].to_value());
+    for i in 0 .. 4 {
+        model.set_value(&iter, i, &data[i as usize].to_value());
+    }
 }
 
 pub fn liststore_to_parols(model: &ListStore) -> parolrs::core::Parols {
-    let iter = match model.get_iter_first() {
-        Some(iter) => iter,
-        None => return parolrs::core::Parols::new(), // empty database
-    };
     let mut parols = parolrs::core::Parols::new();
 
-    loop {
-        let application = match model.get_value(&iter, 0).get::<String>() {
-            Some(string) => string,
-            None => panic!("Cannot convert to String !"),
-        };
-        let username    = match model.get_value(&iter, 1).get::<String>() {
-            Some(string) => string,
-            None => panic!("Cannot convert to String !"),
-        };
-        let password    = match model.get_value(&iter, 2).get::<String>() {
-            Some(string) => string,
-            None => panic!("Cannot convert to String !"),
-        };
-        let notes       = match model.get_value(&iter, 3).get::<String>() {
-            Some(string) => string,
-            None => panic!("Cannot convert to String !"),
-        };
+    if let Some(iter) = model.get_iter_first() {
+        loop {
+            let mut data = Vec::with_capacity(4);
+            for i in 0..4 {
+                data.push(model.get_value(&iter, i as i32).get::<String>().unwrap());
+            }
 
-        let parol = parolrs::core::Parol::new_with_arguments(
-            &application,
-            &username,
-            &password,
-            &notes,
-        );
+            parols.push(
+                parolrs::core::Parol::new_with_arguments(
+                    &data[0], &data[1], &data[2], &data[3]
+                )
+            );
 
-        parols.push(parol);
-
-        if model.iter_next(&iter) == false {
-            break;
+            if model.iter_next(&iter) == false { break; }
         }
     }
 
